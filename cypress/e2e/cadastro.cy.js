2a2b196b1dfdf612546d1b09d1f7af077e0b1ecb
fabioaraujoqa <fabio.araujo.qa@gmail.com>
2026-07-01 22:27:41 -0300

    Page Objects

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -1,9 +1,11 @@
 const faker = require('faker-br');
+import CadastroPage from '../support/pages/cadastro-page'
 
 describe('Funcionalidade: Cadastro no Hub de Leitura', () => {
 
   beforeEach(() => {
-    cy.visit("register.html")
+    //cy.visit("register.html")
+    CadastroPage.visitarPaginaCadastro()
   });
 
   it('Deve fazer o cadastro com campos obrigatórios', () => {
@@ -22,12 +24,27 @@ describe('Funcionalidade: Cadastro no Hub de Leitura', () => {
     cy.get('#user-name').should('contain', nome)
   });
 
-  it.only('Deve bloquear cadastro quando o email estiver repetido', () => {
+  it('Deve bloquear cadastro quando o email estiver repetido', () => {
     let email = `scania${Date.now()}@gmail.com`
     cy.preencherCadastro('Fabio Araujo', email, 'teste@123', 'teste@123') //Cadastrando com um email válido
     cy.preencherCadastro('Fabio Araujo', email, 'teste@123', 'teste@123') //Tentando cadastrar novamente com o mesmo email
     cy.get('#alert-container').should('contain', 'Erro ao criar conta.')
     
+  });
+
+  it('Deve fazer cadastro com sucesso - Usando Page Objects', () => {
+    let email = `scania${Date.now()}@gmail.com`
+    let nome = 'Fábio Araújo'
+    CadastroPage.preencherCadastro(nome, email, '11-987654321', 'Senha@123', 'Senha@123')
+    cy.url().should('include', 'dashboard')
+    cy.get('.fw-bold').should('contain', nome)
+  });
+
+    it.only('Deve validar fluxo com senhas diferentes', () => {
+    let email = `scania${Date.now()}@gmail.com`
+    let nome = 'Fábio Araújo'
+    CadastroPage.preencherCadastro(nome, email, '11-987654321', 'Senha@123', 'Senha@321')
+    cy.contains('Senhas não coincidem').should('exist')
   });
 
 });
--- cypress/e2e/catalogo.cy.js
+++ cypress/e2e/catalogo.cy.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress"/>
+import catalogo from "../fixtures/livros.json"
 
 describe('Funcionalidade: Catálogo do Hub de Leitura', () => {
 
@@ -9,8 +10,39 @@ describe('Funcionalidade: Catálogo do Hub de Leitura', () => {
     it('Deve fazer uma busca de um livro com sucesso', () => {
         cy.get('#search-input').type('Dom Quixote')
         cy.get('.card-title').should('contain', 'Dom Quixote')
-        
-        
     });
-    
+
+    it('Deve validar livro por nome fixo usando contain', () => {
+        let livro = 'Dom Quixote'
+        cy.get('#search-input').type(livro)
+        cy.contains(livro).should('exist')
+    });
+
+    it('Buscar um livro por nome fixo usando contain', () => {
+        let livro = 'Dom Quixote'
+        cy.contains(livro).click()
+        cy.contains(livro).should('exist')
+    });
+
+    it('Deve buscar o primeiro livro da tela', () => {
+        cy.get('[class="btn btn-outline-info btn-sm view-details"]').first().click()
+        cy.get('#add-to-cart-btn').click()
+        cy.contains('Livro adicionado à cesta com sucesso')
+
+    });
+
+    it('Deve buscar o ultimo livro da tela', () => {
+        cy.get('[class="btn btn-outline-info btn-sm view-details"]').last().click()
+        cy.get('#add-to-cart-btn').click()
+        cy.contains('Livro adicionado à cesta com sucesso')
+
+    });
+
+    it('Deve buscar o terceiro livro da tela', () => {
+        cy.get('[class="btn btn-outline-info btn-sm view-details"]').eq(2).click()
+        cy.get('#add-to-cart-btn').click()
+        cy.contains('Livro adicionado à cesta com sucesso')
+    });
+
+
 });
