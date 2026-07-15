8f3486d12b874e1b19328300f669f5cbb12e8e82
fabioaraujoqa <fabio.araujo.qa@gmail.com>
2026-07-15 16:10:07 -0300

    new workflow

--- cypress/e2e/catalogo.cy.js
+++ cypress/e2e/catalogo.cy.js
@@ -9,7 +9,7 @@ describe('Funcionalidade: Catálogo do Hub de Leitura', () => {
 
     it('Deve fazer uma busca de um livro com sucesso', () => {
         cy.get('#search-input').type('Dom Quixote')
-        cy.get('.card-title').should('contain', 'Falha de teste')
+        cy.get('.card-title').should('contain', 'Dom Quixote')
     });
 
     it('Deve validar livro por nome fixo usando contain', () => {
